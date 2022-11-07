78baa2fffb5c16680b2874e77fa68afde36c91c1
David <dfl8@aluno.ifal.edu.br>
2022-11-07 20:01:45 -0300

    feat: server listening

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import Fastify from 'fastify';
+
+const start = async () => {
+
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    await fastify.listen({port: 3333})
+
+}
+
+start()
