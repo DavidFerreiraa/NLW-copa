47c7e0a17a52ad18ed5f24406f2aeab73fb0a8ed
David <dfl8@aluno.ifal.edu.br>
2022-11-13 20:50:17 -0300

    feat: security adjusts and ERD views added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
@@ -11,12 +12,16 @@ const start = async () => {
         logger: true,
     })
 
+    await fastify.register(cors, {
+        origin: true,
+    })
+
     fastify.get('/pools/count', async (request, response) => {
         const count = await prisma.pool.count()
         return { count }
     })
 
-    await fastify.listen({port: 3333})
+    await fastify.listen({port: 3333, host: '0.0.0.0'}) //host 0.0.0.0 is used to better performance in android or ios apps
  
 }
 
