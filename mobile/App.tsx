c6ddd5dec87b7884bd44b4a71fed6b5e4c11bca9
David <dfl8@aluno.ifal.edu.br>
2022-11-15 23:11:39 -0300

    feat: Fonts added

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Text, Center } from 'native-base';
+import { StatusBar } from "expo-status-bar";
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { NativeBaseProvider, Text, Center } from "native-base";
+
+import { THEME } from "./src/styles/THEME";
 
 export default function App() {
-  return (
-      <NativeBaseProvider>
-          <Center flex={1} bgColor="#000000">
-              <Text color="#ffffff" fontSize={24}>Open up App.js to start working on your app!</Text>
-              <StatusBar style="auto" />
-          </Center>
-      </NativeBaseProvider>
-  );
+
+    const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
+
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <Center flex={1} bgColor="gray.900">
+                <Text color="#ffffff" fontSize={24}>
+                    Hello React Native
+                </Text>
+                <StatusBar style="auto" />
+            </Center>
+        </NativeBaseProvider>
+    );
 }
