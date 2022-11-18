d66258acbcb38a1480b87ac0c1cda91faaddb0cb
David <dfl8@aluno.ifal.edu.br>
2022-11-17 21:02:43 -0300

    feat: Post/count routes and zod verification added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,62 @@
-import Fastify from 'fastify';
-import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client';
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
+
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-    log: ['query'],
-})
+    log: ["query"],
+});
 
 const start = async () => {
-
     const fastify = Fastify({
         logger: true,
-    })
+    });
 
     await fastify.register(cors, {
         origin: true,
-    })
+    });
 
-    fastify.get('/pools/count', async (request, response) => {
-        const count = await prisma.pool.count()
+    fastify.get("/pools/count", async () => {
+        const count = await prisma.pool.count();
+        return { count };
+    });
+
+    fastify.get("/users/count", async () => {
+        const count = await prisma.user.count()
+
         return { count }
     })
 
-    await fastify.listen({port: 3333, /*host: '0.0.0.0'*/ }) //host 0.0.0.0 is used to better performance in android or ios apps
- 
-}
+    fastify.get("/guesses/count", async () => {
+        const count = await prisma.guess.count()
 
-start()
+        return { count }
+    })
+
+    fastify.post("/pools", async (request, response) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        const { title } = createPoolBody.parse(request.body);
+
+        const generateCode = new ShortUniqueId({ length: 6 });
+
+        const code = String(generateCode()).toUpperCase()
+
+        const newPool = await prisma.pool.create({
+            data: {
+                title,
+                code,
+            },
+        });
+
+        return response.status(201).send({ newPool });
+    });
+
+    await fastify.listen({ port: 3333 /*host: '0.0.0.0'*/ }); //host 0.0.0.0 is used to better performance in android or ios apps
+};
+
+start();
