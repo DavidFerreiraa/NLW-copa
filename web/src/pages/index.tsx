a3b1d2fa77783b3363e4d34bbf00849b122f7aa7
David <dfl8@aluno.ifal.edu.br>
2022-11-18 00:25:54 -0300

    feat: Style finalized

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,46 +6,54 @@ import iconCheckImg from "../assets/icon-check.svg";
 
 export default function Home() {
     return (
-        <div>
+        <div className="max-w-[1124px] mx-auto h-screen grid grid-cols-2 items-center gap-28">
             <main>
                 <Image src={logoImg} alt="NLW COPA" />
-                <h1>
+                <h1 className="mt-14 text-white text-5xl leading-tight font-bold">
                     Crie seu próprio bolão da copa e compartilhe entre amigos!
                 </h1>
-                <div>
+                <div className="mt-10 flex items-center gap-2">
                     <Image
                         src={avatarImg}
                         alt="Users example avatar"
                         quality={100}
                     />
-                    <strong>
-                        <span>+12.592</span> pessoas já estão usando
+                    <strong className="text-gray-100 text-xl">
+                        <span className="text-ignite-500">+12.592</span> pessoas
+                        já estão usando
                     </strong>
                 </div>
-                <form>
+                <form className="mt-10 flex gap-2">
                     <input
+                        className="flex-1 py-4 px-6 rounded bg-gray-800 border border-gray-600 placeholder:text-sm"
                         type="text"
                         required
                         placeholder="Qual nome do seu bolão?"
                     />
-                    <button type="submit">CRIAR MEU BOLÃO</button>
+                    <button
+                        className="bg-yellow-500 py-4 px-6 rounded font-bold text-gray-900 text-sm hover:bg-yellow-700"
+                        type="submit"
+                    >
+                        CRIAR MEU BOLÃO
+                    </button>
                 </form>
-                <p>
+                <p className="mt-4 text-gray-300 text-sm leading-relaxed">
                     Após criar seu bolão, você receberá um código único que
                     poderá usar para convidar outras pessoas 🚀
                 </p>
-                <div>
-                    <div>
+                <div className="mt-10 pt-10 flex border-t border-gray-600 justify-around items-center text-gray-100">
+                    <div className="flex items-center gap-6">
                         <Image src={iconCheckImg} alt="Check icon" />
-                        <div>
-                            <span>+2.034</span>
+                        <div className="flex flex-col">
+                            <span className="text-2xl font-bold">+2.034</span>
                             <span>Bolões criados</span>
                         </div>
                     </div>
-                    <div>
+                    <div className="w-px h-12 bg-gray-800"/>
+                    <div className="flex items-center gap-6">
                         <Image src={iconCheckImg} alt="Check icon" />
-                        <div>
-                            <span>+2.034</span>
+                        <div className="flex flex-col">
+                            <span className="text-2xl font-bold">+2.034</span>
                             <span>Palpites enviados</span>
                         </div>
                     </div>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,19 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: 'Roboto, sans-serif', //Default font will be Roboto.
-      },
-      colors: {
-        nlw: {
-          900: '#121214',
-        }
-      }
+    content: ["./src/**/*.tsx"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: "Roboto, sans-serif", //Default font will be Roboto.
+            },
+            backgroundImage:{
+                app: 'url(/app-bg.png)',
+            },
+            colors: {
+                ignite: {
+                    500: "#129E57",
+                },
+                gray: {
+                    100: "#E1E1E6",
+                    300: "#8D8D99",
+                    600: "#323238",
+                    800: "#202024",
+                    900: "#121214",
+                },
+                yellow: {
+                    500: "#F7DD43",
+                    700: "#E5CD3D",
+                },
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
