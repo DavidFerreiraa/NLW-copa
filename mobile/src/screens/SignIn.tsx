28c3a724a84d2a811df4a25ce4614096a8baeb4f
David <dfl8@aluno.ifal.edu.br>
2022-11-20 18:53:43 -0300

    feat: SignIn screen finalized

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,14 +1,16 @@
-import { Center, Icon } from "native-base";
+import { Center, Icon, Text } from "native-base";
 import NLWLogo from "../assets/logo.svg";
 import Button from "../components/Button";
 import { Fontisto } from "@expo/vector-icons";
 
 export function SignIn() {
     return (
-        <Center flex={1} bgColor="gray.900">
+        <Center flex={1} bgColor="gray.900" p={7}>
             <NLWLogo width={212} height={40} />
             <Button
+                type="SECONDARY"
                 title="ENTRAR COM GOOGLE"
+                mt={12}
                 leftIcon={
                     <Icon
                         as={Fontisto}
@@ -17,8 +19,17 @@ export function SignIn() {
                         size="md"
                     />
                 }
-                type="SECONDARY"
             />
+            <Text
+                fontSize="sm"
+                fontFamily="mono"
+                color="gray.300"
+                textAlign="center"
+                mt={4}
+            >
+                Não utilizamos nenhuma informação além{'\n'}do seu e-mail para a
+                criação de sua conta.
+            </Text>
         </Center>
     );
 }
