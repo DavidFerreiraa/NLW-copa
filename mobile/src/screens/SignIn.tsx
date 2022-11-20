c42b2467485d2d458f2bf2093e0367a82472d0a9
David <dfl8@aluno.ifal.edu.br>
2022-11-20 18:14:32 -0300

    feat: NLWLogo svg added as a component

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,12 +1,11 @@
 import { Center, Text } from "native-base";
-
+import NLWLogo from '../assets/logo.svg';
 
 export function SignIn() {
     return (
         <Center flex={1} bgColor="gray.900">
-            <Text color="#ffffff" fontSize={24} fontFamily="heading">
-                Hello React Native
-            </Text>
+            <NLWLogo />
+            
         </Center>
     );
 }
