36261ac3ca8c503814968203319a49dabdacf1d5
David <dfl8@aluno.ifal.edu.br>
2022-11-21 00:04:47 -0300

    feat: NewPool screen added

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,7 +9,7 @@ import { NativeBaseProvider, StatusBar } from "native-base";
 import { THEME } from "./src/styles/THEME";
 
 import Loading from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
+import { NewPool } from "./src/screens/NewPool";
 import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
@@ -22,7 +22,7 @@ export default function App() {
     return (
         <NativeBaseProvider theme={THEME}>
             <AuthContextProvider>
-                {fontsLoaded ? <SignIn /> : <Loading />}
+                {fontsLoaded ? <NewPool /> : <Loading />}
                 <StatusBar
                     barStyle="light-content"
                     backgroundColor="transparent"
