8d61991eacd73c264f4b14ce211babead8445408
David <dfl8@aluno.ifal.edu.br>
2022-11-21 22:34:49 -0300

    feat: poll with a owner & poll without owner verif

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -20,15 +20,34 @@ export async function poolRoutes(fastify: FastifyInstance){
         const generateCode = new ShortUniqueId({ length: 6 });
 
         const code = String(generateCode()).toUpperCase();
+        
+        try { //Create a poll when a authenticated user exists
+            await request.jwtVerify()
 
-        const newPool = await prisma.pool.create({
-            data: {
-                title,
-                code,
-            },
-        });
+            const newPool = await prisma.pool.create({
+                data: {
+                    title,
+                    code,
+                    ownerId: request.user.sub,
+                    Participant: {
+                        create: {
+                            userId: request.user.sub,
+                        }
+                    }
+                },
+            });
 
-        return response.status(201).send({ newPool });
+        } catch { //Create a poll when a authenticated user don't exist
+            const newPool = await prisma.pool.create({
+                data: {
+                    title,
+                    code,
+                },
+            });
+        }
+
+
+        return response.status(201).send({ code });
     });
 
 }
