1edb99bb9099c2b5aa05eeba213f11ba9f449cb0
David <dfl8@aluno.ifal.edu.br>
2022-11-29 22:01:20 -0300

    fix: add a request of one poll in other - bug fix

--- mobile/src/components/Game.tsx
+++ mobile/src/components/Game.tsx
@@ -1,18 +1,20 @@
-import { Button, HStack, Text, useTheme, VStack } from 'native-base';
-import { X, Check } from 'phosphor-react-native';
-import { getName } from 'country-list';
-import dayjs from 'dayjs';
-import ptBR from 'dayjs/locale/pt-br';
+import { Button, HStack, Text, useTheme, useToast, VStack } from "native-base";
+import { X, Check } from "phosphor-react-native";
+import { getName } from "country-list";
+import dayjs from "dayjs";
+import ptBR from "dayjs/locale/pt-br";
 
-import { Team } from './Team';
+import { Team } from "./Team";
+import { api } from "../services/api";
+import { useState } from "react";
 
 interface GuessProps {
-  id: string;
-  gameId: string;
-  createdAt: string;
-  participantId: string;
-  firstTeamPoints: number;
-  secondTeamPoints: number;
+    id: string;
+    gameId: string;
+    createdAt: string;
+    participantId: string;
+    firstTeamPoints: number;
+    secondTeamPoints: number;
 }
 
 export interface GameProps {
@@ -23,101 +25,179 @@ export interface GameProps {
     secondTeamPointsFinalResult: number;
     guess: null | GuessProps;
     date: Date;
-};
+}
 
 interface Props {
-  data: GameProps;
-  onGuessConfirm: () => void;
-  setFirstTeamPoints: (value: string) => void;
-  setSecondTeamPoints: (value: string) => void;
-  isLoading: boolean;
-};
+    data: GameProps;
+    onGuessConfirm: () => void;
+    isLoading: boolean;
+    poolId: string;
+    gameId: string;
+}
 
-export function Game({ data, setFirstTeamPoints, setSecondTeamPoints, onGuessConfirm, isLoading }: Props) {
+export function Game({
+    data,
+    onGuessConfirm,
+    isLoading,
+    poolId,
+    gameId,
+}: Props) {
+    const { colors, sizes } = useTheme();
+    const toast = useToast();
+    const [firstTeamPoints, setFirstTeamPoints] = useState<string>("");
+    const [secondTeamPoints, setSecondTeamPoints] = useState<string>("");
 
-  const { colors, sizes } = useTheme();
-  const when = dayjs(data.date).locale(ptBR).format("DD [de] MMMM [de] YYYY [às] HH:00[hrs]")
-  const isPassed = dayjs(data.date).isBefore(dayjs());
+    const when = dayjs(data.date)
+        .locale(ptBR)
+        .format("DD [de] MMMM [de] YYYY [às] HH:00[hrs]");
+    const isPassed = dayjs(data.date).isBefore(dayjs());
 
-  return (
-      <VStack
-          w="full"
-          bgColor="gray.800"
-          rounded="sm"
-          alignItems="center"
-          borderBottomWidth={3}
-          borderBottomColor={isPassed ? "gray.600" : "yellow.500"}
-          mb={3}
-          p={4}
-      >
-          <Text
-              color={isPassed ? "gray.600" : "gray.100"}
-              fontFamily="heading"
-              fontSize="sm"
-          >
-              {getName(data.firstTeamCountryCode)} vs.{" "}
-              {getName(data.secondTeamCountryCode)}
-          </Text>
+    async function handleGuessConfirm(poolId: string, gameId: string) {
+        try {
+            if (!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
+                return toast.show({
+                    title: "Informe o placar para ambos os times!",
+                    placement: "top",
+                    bgColor: "yellow.500",
+                });
+            }
 
-          <Text color={isPassed ? "gray.600" : "gray.200"} fontSize="xs">
-              {when}
-          </Text>
+            await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+                firstTeamPoints: Number(firstTeamPoints),
+                secondTeamPoints: Number(secondTeamPoints),
+            });
 
-          <HStack
-              mt={4}
-              w="full"
-              justifyContent="space-between"
-              alignItems="center"
-          >
-              <Team
-                  code={data.firstTeamCountryCode}
-                  position="right"
-                  onChangeText={setFirstTeamPoints}
-              />
+            toast.show({
+                title: "Palpite enviado!",
+                placement: "top",
+                bgColor: "green.500",
+            });
+        } catch (error) {
+            if (
+                error.response?.data?.message ===
+                "You're not allowed to create a guess inside this pool."
+            ) {
+                toast.show({
+                    title: "Você não pode criar um palpite dentro desse bolão.",
+                    placement: "top",
+                    bgColor: "red.500",
+                });
+            } else if (
+                error.response?.data?.message ===
+                "You alredy send a guess to this game on this pool."
+            ) {
+                toast.show({
+                    title: "Você já enviou um palpite para esse bolão.",
+                    placement: "top",
+                    bgColor: "red.500",
+                });
+            } else if (error.response?.data?.message === "Game not found.") {
+                toast.show({
+                    title: "Esse jogo não existe dentro desse bolão.",
+                    placement: "top",
+                    bgColor: "red.500",
+                });
+            } else if (
+                error.response?.data?.message ===
+                "You cannot send guesses after the game date."
+            ) {
+                toast.show({
+                    title: "Esse jogo já aconteceu!",
+                    placement: "top",
+                    bgColor: "red.500",
+                });
+            } else {
+                toast.show({
+                    title: "Não foi possível enviar o palpite.",
+                    placement: "top",
+                    bgColor: "red.500",
+                });
+            }
+        }
+    }
 
-              {data.firstTeamPointsFinalResult != null ? (
-                  <Text color="gray.300">
-                      {data.firstTeamPointsFinalResult}
-                  </Text>
-              ) : null}
-              <X color={colors.gray[300]} size={sizes[6]} />
-              {data.secondTeamPointsFinalResult != null ? (
-                  <Text color="gray.300">
-                      {data.secondTeamPointsFinalResult}
-                  </Text>
-              ) : null}
+    return (
+        <VStack
+            w="full"
+            bgColor="gray.800"
+            rounded="sm"
+            alignItems="center"
+            borderBottomWidth={3}
+            borderBottomColor={isPassed ? "gray.600" : "yellow.500"}
+            mb={3}
+            p={4}
+        >
+            <Text
+                color={isPassed ? "gray.600" : "gray.100"}
+                fontFamily="heading"
+                fontSize="sm"
+            >
+                {getName(data.firstTeamCountryCode)} vs.{" "}
+                {getName(data.secondTeamCountryCode)}
+            </Text>
 
-              <Team
-                  code={data.secondTeamCountryCode}
-                  position="left"
-                  onChangeText={setSecondTeamPoints}
-              />
-          </HStack>
+            <Text color={isPassed ? "gray.600" : "gray.200"} fontSize="xs">
+                {when}
+            </Text>
 
-          {!data.guess && (
-              <Button
-                  size="xs"
-                  w="full"
-                  bgColor="green.500"
-                  mt={4}
-                  onPress={onGuessConfirm}
-                  isLoading={isLoading}
-                  isDisabled={isPassed}
-              >
-                  <HStack alignItems="center">
-                      <Text
-                          color="white"
-                          fontSize="xs"
-                          fontFamily="heading"
-                          mr={3}
-                      >
-                          CONFIRMAR PALPITE
-                      </Text>
+            <HStack
+                mt={4}
+                w="full"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Team
+                    code={data.firstTeamCountryCode}
+                    position="right"
+                    onChangeText={setFirstTeamPoints}
+                />
 
-                      <Check color={colors.white} size={sizes[4]} />
-                  </HStack>
-              </Button>
-          )}
-      </VStack>
-  );
+                {data.firstTeamPointsFinalResult != null ? (
+                    <Text color="gray.300">
+                        {data.firstTeamPointsFinalResult}
+                    </Text>
+                ) : null}
+                <X color={colors.gray[300]} size={sizes[6]} />
+                {data.secondTeamPointsFinalResult != null ? (
+                    <Text color="gray.300">
+                        {data.secondTeamPointsFinalResult}
+                    </Text>
+                ) : null}
+
+                <Team
+                    code={data.secondTeamCountryCode}
+                    position="left"
+                    onChangeText={setSecondTeamPoints}
+                />
+            </HStack>
+
+            {!data.guess && (
+                <Button
+                    size="xs"
+                    w="full"
+                    bgColor="green.500"
+                    mt={4}
+                    onPress={async () => {
+                        await handleGuessConfirm(poolId, gameId),
+                        onGuessConfirm()
+                    }}
+                    isLoading={isLoading}
+                    isDisabled={isPassed}
+                >
+                    <HStack alignItems="center">
+                        <Text
+                            color="white"
+                            fontSize="xs"
+                            fontFamily="heading"
+                            mr={3}
+                        >
+                            CONFIRMAR PALPITE
+                        </Text>
+
+                        <Check color={colors.white} size={sizes[4]} />
+                    </HStack>
+                </Button>
+            )}
+        </VStack>
+    );
 }
--- mobile/src/components/Guesses.tsx
+++ mobile/src/components/Guesses.tsx
@@ -1,8 +1,5 @@
-import { GrantType } from "expo-auth-session";
 import { useToast, FlatList } from "native-base";
-import { ItemClick } from "native-base/lib/typescript/components/composites/Typeahead/useTypeahead/types";
 import { useEffect, useState } from "react";
-import { Share } from "react-native";
 import { api } from "../services/api";
 import { EmptyMyPoolList } from "./EmptyMyPoolList";
 import { Game, GameProps } from "./Game";
@@ -15,8 +12,6 @@ interface Props {
 
 export function Guesses({ poolId, code }: Props) {
     const [ isLoading, setIsLoading ] = useState<boolean>(false);
-    const [ firstTeamPoints, setFirstTeamPoints ] = useState<string>('')
-    const [ secondTeamPoints, setSecondTeamPoints] = useState<string>('');
     const [ games, setGames ] = useState<GameProps[]>([]);
 
     const toast = useToast();
@@ -41,73 +36,6 @@ export function Guesses({ poolId, code }: Props) {
         }
     }
 
-    async function handleGuessConfirm(gameId: string) {
-        try {
-            if(!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
-                return toast.show({
-                    title: "Informe o placar para ambos os times!",
-                    placement: "top",
-                    bgColor: "yellow.500",
-                });
-            }
-
-            await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
-                firstTeamPoints: Number(firstTeamPoints),
-                secondTeamPoints: Number(secondTeamPoints),
-            });
-
-            toast.show({
-                title: "Palpite enviado!",
-                placement: "top",
-                bgColor: "green.500",
-            });
-
-            fetchGames();
-
-        } catch (error) {
-            if (
-                error.response?.data?.message ===
-                "You're not allowed to create a guess inside this pool."
-            ) {
-                toast.show({
-                    title: "Você não pode criar um palpite dentro desse bolão.",
-                    placement: "top",
-                    bgColor: "red.500",
-                });
-            } else if (
-                error.response?.data?.message ===
-                "You alredy send a guess to this game on this pool."
-            ) {
-                toast.show({
-                    title: "Você já enviou um palpite para esse bolão.",
-                    placement: "top",
-                    bgColor: "red.500",
-                });
-            } else if (error.response?.data?.message === "Game not found.") {
-                toast.show({
-                    title: "Esse jogo não existe dentro desse bolão.",
-                    placement: "top",
-                    bgColor: "red.500",
-                });
-            } else if (
-                error.response?.data?.message ===
-                "You cannot send guesses after the game date."
-            ) {
-                toast.show({
-                    title: "Esse jogo já aconteceu!",
-                    placement: "top",
-                    bgColor: "red.500",
-                });
-            } else {
-                toast.show({
-                    title: "Não foi possível enviar o palpite.",
-                    placement: "top",
-                    bgColor: "red.500",
-                });
-            }
-        }
-    }
-
     useEffect(() => {
         fetchGames();
     }, [poolId]);
@@ -123,9 +51,9 @@ export function Guesses({ poolId, code }: Props) {
             renderItem={({item}) => (
                 <Game
                     data={item}
-                    setFirstTeamPoints={setFirstTeamPoints}
-                    setSecondTeamPoints={setSecondTeamPoints}
-                    onGuessConfirm={() => handleGuessConfirm(item.id)}
+                    onGuessConfirm={() => fetchGames()}
+                    poolId={poolId}
+                    gameId={item.id}
                     isLoading={isLoading}
                 />
             )
